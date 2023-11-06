1fb8b9984b124e5fdc5e9e18734156e5fae78cd5
vivasuk683 <vivas@DESKTOP-BE431TS>
2023-11-06 04:30:03 +0300

    комит 2

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include "../lib_example/example.h"
-
+#include "../lib_stack/stack.h"
 int main() {
   int a = 1, b = 4; 
   float result;
@@ -29,6 +29,11 @@ int main() {
   ExampleClass<int> obj(10);
   obj.setRandValues(-50, 30);
   std::cout << obj << std::endl;
-
+  Stack<int> test(10);
+  test.push(5);
+  test.push(6);
+  test.push(7);
+  test.pop();
+  test.print();
   return 0;
 }
